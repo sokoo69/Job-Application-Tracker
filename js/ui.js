0398e4be06a0e0cbb982e73735bd1c2237db5f49
saykot biswas shawon <shawon.saykot2023@gmail.com>
2026-02-22 23:46:28 +0600

    element from data

---
+++ js/ui.js
@@ -0,0 +1,7 @@
+const jobsList = document.getElementById('jobsList');
+const emptyState = document.getElementById('emptyState');
+const totalCountEl = document.getElementById('totalCount');
+const interviewCountEl = document.getElementById('interviewCount');
+const rejectedCountEl = document.getElementById('rejectedCount');
+const jobsCountEl = document.getElementById('jobsCount');
+const tabBtns = document.querySelectorAll('.tab-btn');
